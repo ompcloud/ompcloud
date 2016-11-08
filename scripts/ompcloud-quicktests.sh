96aaced5ea5bf4cdd0233bd1e22f7c3850124835
Hervé Yviquel <hyviquel@gmail.com>
2016-11-08 00:01:49 -0200

    compile and test all supported

--- scripts/ompcloud-quicktests.sh
+++ scripts/ompcloud-quicktests.sh
@@ -10,8 +10,9 @@ export CC=$LLVM_BUILD/bin/clang
 mkdir $UNIBENCH_BUILD_TEST
 cd $UNIBENCH_BUILD_TEST
 cmake $UNIBENCH_SRC -DCMAKE_BUILD_TYPE=Release -DRUN_TEST=ON
-make experiments
+make supported
 
 # Run experiments
 #ctest -I 2,2,,2,6,21,23,27,29,33,34
-ctest -I 2,2,,2,6,21,29,34
+#ctest -I 2,2,,2,6,21,29,33,34 --output-on-failure
+ctest --output-on-failure
