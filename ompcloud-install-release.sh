7bb64c0e5fcbf6b513989ff683d382aca1c109f8
Hervé Yviquel <hyviquel@gmail.com>
2017-05-15 11:52:02 -0300

    install sbt after java to avoid error during the configuration

--- ompcloud-install-release.sh
+++ ompcloud-install-release.sh
@@ -131,15 +131,6 @@ export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/"
 $SUDO apt-get update && \
   $SUDO apt-get install -y apt-utils apt-transport-https
 
-sbt_list="/etc/apt/sources.list.d/sbt.list"
-if [ -f "$sbt_list" ]
-then
-	echo "Sbt repository is already in apt sources list."
-else
-  echo "deb https://dl.bintray.com/sbt/debian /" | $SUDO tee -a $sbt_list
-  $SUDO apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
-fi
-
 $SUDO apt-get clean all && \
   $SUDO apt-get update && \
   $SUDO apt-get upgrade -y
@@ -147,8 +138,19 @@ $SUDO apt-get clean all && \
 # Default Java 9 does not seem to be compatible with SBT
 $SUDO apt-get install -y gcc g++ cmake libxml2-dev uuid-dev \
   libprotobuf-dev protobuf-compiler libgsasl7-dev libkrb5-dev \
-  libboost-all-dev libssh-dev libelf-dev libffi-dev sbt \
-  openssh-server git openjdk-8-jre-headless
+  libboost-all-dev libssh-dev libelf-dev libffi-dev git \
+  openssh-server openjdk-8-jre-headless
+
+sbt_list="/etc/apt/sources.list.d/sbt.list"
+if [ -f "$sbt_list" ]
+then
+	echo "Sbt repository is already in apt sources list."
+else
+  echo "deb https://dl.bintray.com/sbt/debian /" | $SUDO tee -a $sbt_list
+  $SUDO apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+  $SUDO apt-get update
+fi
+$SUDO apt-get install -y sbt
 
 if [ $OP == "-i" ] || [ $OP == "-ri" ]; then
     $SUDO apt-get install -y  wget python-pip
@@ -300,6 +302,6 @@ else
     mkdir $UNIBENCH_BUILD
     cd $UNIBENCH_BUILD
     export CC=$LLVM_BUILD/bin/clang
-    cmake $UNIBENCH_SRC -DCMAKE_BUILD_TYPE=Release  -DRUN_TEST=OFF -DRUN_BENCHMARK=ON -DCMAKE_BUILD_TYPE=Release
+    cmake $UNIBENCH_SRC -DCMAKE_BUILD_TYPE=Release -DRUN_TEST=OFF -DRUN_BENCHMARK=ON -DCMAKE_BUILD_TYPE=Release
     make experiments
 fi
