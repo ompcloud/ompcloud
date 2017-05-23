bbfb4824e3e966d661f01492d51d7defaf176c4c
Hervé Yviquel <hyviquel@gmail.com>
2017-05-23 15:56:23 -0300

    fix setup env script

--- release/ompcloud-setup-env.sh
+++ release/ompcloud-setup-env.sh
@@ -8,5 +8,5 @@ REAL_BASEDIR="$(realpath $BASEDIR)"
 
 echo "Copy OmpCloud Spark library to SBT local repository"
 mkdir -p $HOME/.ivy2/local/
-rm -rf $REAL_BASEDIR/local/org.llvm.openmp
+rm -rf $HOME/.ivy2/local/org.llvm.openmp
 cp -rf $REAL_BASEDIR/local/org.llvm.openmp $HOME/.ivy2/local/
