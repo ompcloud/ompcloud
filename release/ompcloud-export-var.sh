02caaf41ec4caf0cbf051b332d759a147960ad91
Hervé Yviquel <hyviquel@gmail.com>
2017-05-25 13:54:31 -0300

    add detection when sourcing the script to avoid problem

--- release/ompcloud-export-var.sh
+++ release/ompcloud-export-var.sh
@@ -14,3 +14,11 @@ export LD_LIBRARY_PATH="$REAL_BASEDIR/lib:$LD_LIBRARY_PATH"
 # Set OmpCloud environment variables
 export OMPCLOUD_CONF_PATH="$REAL_BASEDIR/conf/cloud_rtl.ini.local"
 export LIBHDFS3_CONF="$REAL_BASEDIR/conf/hdfs-client.xml"
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  echo "ERROR this script need to be sourced"
+  echo "Run 'source $BASEDIR/ompcloud-export-var.sh'"
+else
+  echo "OmpCloud env variables exported for the current shell session"
+  echo "Note it needs to be re-run for future shell session"
+fi
--- release/ompcloud-setup-env.sh
+++ release/ompcloud-setup-env.sh
@@ -6,7 +6,14 @@ function realpath { echo $(cd $(dirname $1); pwd)/$(basename $1); }
 BASEDIR=$(dirname "$0")
 REAL_BASEDIR="$(realpath $BASEDIR)"
 
-echo "Copy OmpCloud Spark library to SBT local repository"
+if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
+  echo "ERROR this script should not to be sourced"
+  echo "Run '$BASEDIR/ompcloud-setup-env.sh'"
+  return
+fi
+
 mkdir -p $HOME/.ivy2/local/
 rm -rf $HOME/.ivy2/local/org.llvm.openmp
 cp -rf $REAL_BASEDIR/local/org.llvm.openmp $HOME/.ivy2/local/
+
+echo "OmpCloud Spark library copied to SBT local repository"
